b8475d85177522d8580819a36ee37695cfa8de4e
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-12-17 11:20:10 +0500

    UI Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Header from "../src/components/Header";
+import Balance from "../src/components/Balance";
+import IncomeExpenses from "../src/components/IncomeExpenses";
+import TransactionList from "../src/components/TransactionList";
+import AddTransaction from "../src/components/AddTransactions";
+import "../src/App.css";
 
 const App = () => {
   return (
-    <div>
-      <h2>Working</h2>
-    </div>
-  )
-}
+    <React.Fragment>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
