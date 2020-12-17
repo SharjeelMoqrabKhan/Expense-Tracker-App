36026b7b4ef0ba84c887be0a65e5a3b702c13e9a
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2020-12-17 12:05:14 +0500

    Initialzied Global State

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Balance from "../src/components/Balance";
 import IncomeExpenses from "../src/components/IncomeExpenses";
 import TransactionList from "../src/components/TransactionList";
 import AddTransaction from "../src/components/AddTransactions";
+import { GlobalProvider } from "../src/context/GlobalState";
 import "../src/App.css";
 
 const App = () => {
   return (
-    <React.Fragment>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -16,7 +17,7 @@ const App = () => {
         <TransactionList />
         <AddTransaction />
       </div>
-    </React.Fragment>
+    </GlobalProvider>
   );
 };
 
